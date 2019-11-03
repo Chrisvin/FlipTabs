f4190602cde3be429e5d6883f53c57fd8a9ddc2e
Jem <chrisvin1997@gmail.com>
2019-11-04 00:54:47 +0530

    Update demo app

--- app/src/main/java/com/jem/fliptabsdemo/MainActivity.kt
+++ app/src/main/java/com/jem/fliptabsdemo/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.jem.fliptabsdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.animation.OvershootInterpolator
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.jem.fliptabs.FlipTab
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val stringTemplate = "This is the %s content."
+        textSwitcher.setInAnimation(this@MainActivity, android.R.anim.slide_in_left)
+        textSwitcher.setOutAnimation(this@MainActivity, android.R.anim.slide_out_right)
+
+        fliptab.setTabSelectedListener(object: FlipTab.TabSelectedListener {
+            override fun onTabSelected(isLeftTab: Boolean, tabTextValue: String) {
+                textSwitcher.setText(String.format(stringTemplate, tabTextValue))
+                Toast.makeText(this@MainActivity, (if (isLeftTab) "Left" else "Right") + " tab selected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(isLeftTab: Boolean, tabTextValue: String) {
+                Toast.makeText(this@MainActivity, (if (isLeftTab) "Left" else "Right") + " tab reselected", Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 }
