4e4f843facfecba59c6e29ef900111c55e4a31dc
Jem <chrisvin1997@gmail.com>
2019-11-05 23:50:36 +0530

    Update demo app code

--- app/src/main/java/com/jem/fliptabsdemo/MainActivity.kt
+++ app/src/main/java/com/jem/fliptabsdemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jem.fliptabsdemo
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
@@ -16,6 +17,36 @@ class MainActivity : AppCompatActivity() {
         val stringTemplate = "This is the %s content."
         textSwitcher.setInAnimation(this@MainActivity, android.R.anim.slide_in_left)
         textSwitcher.setOutAnimation(this@MainActivity, android.R.anim.slide_out_right)
+
+        /*
+        val flipTab = FlipTab(this)
+        flipTab.setLeftTabText("Photo")
+        flipTab.setRightTabText("Video")
+
+        //Sets color for both text, background & border
+        flipTab.setOverallColor(Color.BLUE)
+        //Sets color only bg & border
+        flipTab.setHighlightColor(Color.GREEN)
+        //Sets color only for text
+        flipTab.setTextColor(Color.GREEN)
+
+        //Time taken for selected tab to flip
+        flipTab.setFlipAnimationDuration(500)
+        //Time taken for tabs to revert to original state after wobble
+        flipTab.setWobbleReturnAnimationDuration(250)
+        //NOTE: totalAnimationDuration = flipAnimationDuration + wobbleReturnAnimationDuration
+
+        //Set angle upto which the tabs wobble
+        flipTab.setWobbleAngle(3f)
+
+        //Flip the tab (left -> right & vice versa)
+        flipTab.flipTabs()
+        //Selects the left tab (if left tab isn't already selected)
+        flipTab.selectLeftTab(withAnimation = true)
+        //Selects the right tab (if right tab isn't already selected)
+        flipTab.selectRightTab(withAnimation = false)
+        //(Does flip animation when selecting if withAnimation is true, else skips animation)
+         */
 
         fliptab.setTabSelectedListener(object: FlipTab.TabSelectedListener {
             override fun onTabSelected(isLeftTab: Boolean, tabTextValue: String) {
